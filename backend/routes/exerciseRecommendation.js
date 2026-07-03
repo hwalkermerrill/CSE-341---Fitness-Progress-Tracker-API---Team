860c274f643a4a27c2dfe133fdaf7cc666857aa2
brysonwebb98 <brysonwebb98@gmail.com>
2026-07-02 22:52:49 -0600

    Adding POST, PUT and DELETE to exercise recommendations

--- backend/routes/exerciseRecommendation.js
+++ backend/routes/exerciseRecommendation.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Exercise Recommendations route working');
-});
+const exerciseRecommendationsController = require('../controllers/exerciseRecommendations');
 
+// GET REQUEST FOR ALL EXERCISE RECOMMENDATIONS
+router.get('/', exerciseRecommendationsController.getAll);
+
+// GET REQUEST FOR EXERCISE BASED ON ID
+router.get('/:id', exerciseRecommendationsController.getSingle);
+
+// POST REQUEST FOR ADDING EXERCISE RECOMMENDATIONS
+router.post('/', exerciseRecommendationsController.createRecommendation);
+
+// DELETE REQUEST FOR DELETING EXERCISE RECOMMENDATIONS
+router.delete('/:id', exerciseRecommendationsController.deleteRecommendation)
+
+// PUT REQUEST FOR UPDATING EXISTING EXERCISE
+router.put('/:id', exerciseRecommendationsController.updateRecommendation);
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongodb = require('./DB/connection');
 const app = express();
 const routes = require('./routes');
@@ -7,6 +8,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
