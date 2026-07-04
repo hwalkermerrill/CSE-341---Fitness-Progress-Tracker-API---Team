3ac0e5288d0f2ed926107dca5e8b40558eb13b09
RebeccaHanks <han22023@byui.edu>
2026-07-04 12:45:38 -0600

    Add POST route for goals

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { createGoal } = require('../controllers/goals');
+
 router.get('/', (req, res) => {
     res.send('Goals route working');
 });
+
+router.post('/', createGoal);
 
 module.exports = router;
